95e4d92bd43c548c78e6d4dc7106fba73ce82e5a
rickyp72 <rickyp72@googlemail.com>
2016-11-17 14:15:50 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# ossec-elk
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: ossec-elk
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
