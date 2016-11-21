329f9faecafd9da568f4e14a2dcf357b171cbebd
rickyp72 <ricky@email.com>
2016-11-21 12:49:36 +0000

    berkshelf and vagrant initialisation

---
+++ attributes/default.rb
@@ -0,0 +1,11 @@
+
+
+
+
+override['java']['install_flavor'] = 'oracle'
+override['java']['jdk_version'] = '8'
+override['java']['oracle']['accept_oracle_download_terms'] = true
+
+override['logstash']['source']['repo'] = 'git://github.com/elasticsearch/logstash.git'
+# override['logstash']['source']['sha'] = nil
+override['logstash']['source']['java_home'] = '/usr/lib/jvm/java-8-oracle-amd64' # oracle 8 on ubuntu
