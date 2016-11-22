0a765edf525de4c3d842e42f47753275d4e1b748
rickyp72 <rickyp72@googlemail.com>
2016-11-22 15:08:23 +0000

    installs and configure ES now, but plugins notworking

--- recipes/elasticsearch.rb
+++ recipes/elasticsearch.rb
@@ -4,4 +4,26 @@ elasticsearch_user 'elasticsearch'
 
 elasticsearch_install 'elasticsearch' do
   type :package.to_s
+  version "5.0.1"
+  action :install
 end
+
+elasticsearch_configure 'elasticsearch' do
+  allocated_memory '256m'
+  configuration ({
+  'cluster.name' => 'rnb_es',
+  'node.name' => 'rnb-ossec',
+  'network.host' => '_site_'
+})
+end
+
+elasticsearch_service 'elasticsearch' do
+  service_actions [:enable, :start]
+end
+
+
+# elasticsearch_plugin 'head' do
+#   url 'mobz/elasticsearch-head'
+#   action :install
+#   notifies :restart, 'elasticsearch_service[elasticsearch]', :delayed
+# end
